0e9535fa6c2944d9b4a721e649bc15c3dfdafb82
Cristian Pérez García <cristian.perez@cmv.mx>
2019-09-03 17:54:42 -0500

    Se agrega controlador de login.

--- ImporteInventario/App_Start/RouteConfig.cs
+++ ImporteInventario/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ImporteInventario
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Sesion", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ImporteInventario/Controllers/SesionController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImporteInventario.Controllers
+{
+    public class SesionController : Controller
+    {
+        // GET: Sesion
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult IniciarSesion()
+        {
+            try
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public ActionResult CerrarSesion()
+        {
+            try
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
